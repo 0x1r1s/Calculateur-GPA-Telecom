010bc15921f9c74e5b0b62b4643ed63e9e0062e5
Tristan Perrot <tristan.perrot@telecom-paris.fr>
2023-02-26 15:06:39 +0100

    adding global gpa and mean but the mean seems to be false

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
-const elements2 = document.querySelectorAll("[id*=collapse-]");
+const elements = document.querySelectorAll("[id*=collapse-]");
 const ids = [];
 
-elements2.forEach((element) => {
+elements.forEach((element) => {
     const id = element.getAttribute("id");
     const number = id.split("-")[1];
     ids.push(number);
@@ -27,60 +27,90 @@ function convertToGrade(markParam) {
     }
 }
 
-async function fetchValue(num) {
+function addToHtml(when, gpaValue, gradeValue) {
+    const h1Element = document.querySelector("h1");
+    const newElement = document.createElement("p");
+    newElement.innerText = "Ton GPA de " + when + " est: " + gpaValue + " ( Moyenne: " + gradeValue + " )";
+    h1Element.insertAdjacentElement("afterend", newElement);
+}
+
+var allGpaWeightedSum = 0;
+var allGradeWeightedSum = 0;
+var allCoefSum = 0;
+var execution = 0;
+
+async function fetchPerYear(num) {
     try {
-        var marks = [];
+        var grades = [];
+        var gpaGrades = [];
         var coefficients = [];
-        var weightedAverage;
+        var gpaWeightedAverage;
+        var gradeWeightedAverage;
+        
         const response = await fetch(
             "https://synapses.telecom-paris.fr/liste-notes/" + num
         );
         const data = await response.text();
+        
         // Parse the HTML page to a DOM object
         var parser = new DOMParser();
         var htmlDoc = parser.parseFromString(data, "text/html");
+
+        console.log(htmlDoc)
 
         // Select all the <tr> elements from the parsed HTML
         var trElements = htmlDoc.querySelectorAll("tr");
 
         // Iterate over each <tr> element and retrieve the values
-        var i = 0;
         trElements.forEach(function (trElement) {
             if (trElement.children.length > 12) {
                 var mark = parseInt(trElement.children[9].textContent);
                 var coefficient = trElement.children[11].textContent;
                 if (!isNaN(mark) && coefficient.includes("/")) {
-                    marks[i] = convertToGrade(mark);
-                    coefficients[i] = coefficient;
-                    i++;
+                    grades.push(mark);
+                    gpaGrades.push(convertToGrade(mark));
+                    coefficients.push(coefficient);
                 }
             }
         });
-        let sum = 0;
-        let totalCoefficient = 0;
-        for (let i_1 = 0; i_1 < marks.length; i_1++) {
+        
+        // Calculating the GPA
+        let gradesWeightedSum = 0;
+        let gpaWeightedSum = 0;
+        let coefficientsSum = 0;
+        for (let i_1 = 0; i_1 < gpaGrades.length; i_1++) {
             const numerator = parseFloat(coefficients[i_1].split("/")[1]);
-            sum += marks[i_1] * numerator;
-            totalCoefficient += numerator;
+            gradesWeightedSum += grades[i_1] * numerator;
+            gpaWeightedSum += gpaGrades[i_1] * numerator;
+            coefficientsSum += numerator;
         }
-        weightedAverage = Math.round((sum * 100) / totalCoefficient) / 100;
-        // Select the h1 element
-        const h1Element = document.querySelector("h1");
 
-        // Create a new element to hold the text
-        const newElement = document.createElement("p");
-        if (ids[0] == num) {
-            year = "1ère année";
-        } else if (ids[1] == num) {
-            year = "2nde année";
-        } else if (ids[2] == num) {
-            year = "3ème année";
+        gradeWeightedAverage = Math.round((gradesWeightedSum * 100) / coefficientsSum) / 100;
+        gpaWeightedAverage = Math.round((gpaWeightedSum * 100) / coefficientsSum) / 100;
+
+        allGradeWeightedSum += gradesWeightedSum;
+        allGpaWeightedSum += gpaWeightedSum;
+        allCoefSum += coefficientsSum;
+
+        // Determining the year
+        if (ids[ids.length - 1] == num) {
+            when = "de 1ère année";
+        } else if (ids[ids.length - 2] == num) {
+            when = "de 2nde année";
+        } else if (ids[ids.length - 3] == num) {
+            when = "de 3ème année";
         }
-        newElement.innerText = "Ton GPA de " + year + " est: " + weightedAverage;
 
-        // Insert the new element after the h1 element
-        h1Element.insertAdjacentElement("afterend", newElement);
-        return weightedAverage;
+        addToHtml(when, gpaWeightedAverage, gradeWeightedAverage);
+        execution++;
+
+        if (ids.length == execution) {
+            var allGpaWeightedAverage = Math.round((allGpaWeightedSum * 100) / allCoefSum) / 100;
+            var allGradeWeightedAverage = Math.round((allGradeWeightedSum * 100) / allCoefSum) / 100;
+            addToHtml("global", allGpaWeightedAverage, allGradeWeightedAverage);
+        }
+        
+        return gpaWeightedAverage;
     } catch (error) {
         return console.error(error);
     }
@@ -89,6 +119,6 @@ async function fetchValue(num) {
 ids.forEach(
     async (id) => {
         console.log(id);
-        await fetchValue(id)
+        await fetchPerYear(id)
     }
 )
